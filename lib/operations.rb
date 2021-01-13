726b874685c662fee70436c70f89eec8c79973b1
nbyrd9 <nianibyrd@gmail.com>
2021-01-12 21:03:31 -0500

    commit tree 29191bc7eddd4df01470f2c0ffd6a611ff4249ae

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,18 @@
 def unsafe?(speed)
+    if speed == 50
+        return false
+    elsif speed < 40 
+        return true
+    elsif speed > 60
+        return true
+    end
 
 end
 
-
-
-def not_safe?(speed)
-	
+def not_safe? (speed)
+    speed < 40 || speed > 60 ? true : false
 end
-	
+
+
 
 
